ddb99847faf6b307156b2e78e4c7d23029fe2c82
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-05-24 12:57:34 -0500

    completed

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+class CashRegister
+    attr_reader :discount, :items
+    attr_accessor :total, :last_transaction
+
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.total += price * quantity
+            for _ in 0...quantity 
+                self.items << title 
+            end
+        self.last_transaction = price * quantity
+    end
+
+    def apply_discount
+        percent = (100.0 - self.discount) / 100.0 
+        if self.discount != 0
+            self.total *= percent
+            return "After the discount, the total comes to $#{self.total.to_i}."
+        else 
+            return "There is no discount to apply."
+        end
+    end
+    
+    def void_last_transaction
+        self.items.pop()
+        if self.items.length == 0
+            self.total = 0.0
+        else
+            self.total -= self.last_transaction
+        end
+    end
+end
+
+
 
